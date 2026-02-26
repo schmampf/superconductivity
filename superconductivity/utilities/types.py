6e4efe89eafe60974691053c72eec54199c47a6f
schmampf <oliver.irtenkauf@uni-konstanz.de>
2026-02-26 15:58:36 +0100

    more types

--- superconductivity/utilities/types.py
+++ superconductivity/utilities/types.py
@@ -1,10 +1,8 @@
-from typing import TypeAlias
-from typing import Callable
-
-import numpy as np
-from numpy.typing import NDArray
+from typing import Callable, Optional, Sequence, Tuple, TypeAlias, Union
 
 import jax.numpy as jnp
+import numpy as np
+from numpy.typing import NDArray
 
 NDArray64: TypeAlias = NDArray[np.float64]
 JNDArray: TypeAlias = jnp.ndarray
@@ -14,3 +12,10 @@ ModelFunction: TypeAlias = Callable[..., NDArray64]
 
 ModelType: TypeAlias = tuple[ModelFunction, NDArray[np.bool]]
 ParameterType: TypeAlias = tuple[float, tuple[float, float], bool]
+
+
+Number = Union[int, float]
+RGB = Tuple[Number, Number, Number]
+RGBA = Tuple[Number, Number, Number, Number]
+COLOR = Union[RGB, RGBA, Sequence[Number]]
+LIM = Optional[Tuple[Optional[float], Optional[float]]]
