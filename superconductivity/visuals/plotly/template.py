dcb8d6cb6b9197870c7f8e6971d8c2d3dc79da3d
schmampf <oliver.irtenkauf@uni-konstanz.de>
2026-03-11 09:50:44 +0100

    updated template

--- superconductivity/visuals/plotly/template.py
+++ superconductivity/visuals/plotly/template.py
@@ -2,8 +2,16 @@
 
 import plotly.graph_objects as go
 import plotly.io as pio
+from matplotlib.colors import ListedColormap
 
 from superconductivity.style.cpd4 import cmap
+from superconductivity.style.cpd5 import (
+    seeblau100,
+    seegrau35,
+    seegrau65,
+    seegrau100,
+    weiss,
+)
 from superconductivity.utilities.types import COLOR
 
 from .helper import mpl_cmap_to_plotly, mpl_color_to_plotly
@@ -17,35 +25,55 @@ def build_house_template() -> go.layout.Template:
     go.layout.Template
         Plotly template containing project-wide layout and trace defaults.
     """
-    gridcolor_mpl: COLOR = (0, 0, 0, 0.15)
-    gridwidth: float = 1.0
-    backgroundcolor_mpl: COLOR = (1, 1, 1, 1)
+    axiscolor_mpl: COLOR = seegrau100
+    axiscolor = mpl_color_to_plotly(axiscolor_mpl)
 
+    gridcolor_mpl: COLOR = seegrau65
     gridcolor = mpl_color_to_plotly(gridcolor_mpl)
+
+    backgroundcolor_mpl: COLOR = weiss
     backgroundcolor = mpl_color_to_plotly(backgroundcolor_mpl)
 
-    colormap = mpl_cmap_to_plotly(cmap_mpl=cmap())
+    colormap_mpl: ListedColormap = cmap()
+    colormap = mpl_cmap_to_plotly(colormap_mpl)
+
+    gridwidth: float = 1.0
+    axiswidth: float = 1.0
+    linewidth: float = 2.0
+
+    tickformat: str = ".3g"
+    fontsize: float = 14
 
     global_axis = dict(
         showgrid=True,
         gridcolor=gridcolor,
         gridwidth=gridwidth,
         showline=True,
-        linecolor="rgba(0,0,0,0.35)",
-        linewidth=1,
+        linecolor=axiscolor,
+        linewidth=axiswidth,
         zeroline=False,
-        tickformat=".3g",
+        tickformat=tickformat,
     )
-    local_axis = dict(showgrid=True, gridcolor=gridcolor, tickformat=".3g")
+    local_axis = dict(
+        showgrid=True,
+        gridcolor=gridcolor,
+        tickformat=tickformat,
+    )
 
     template = go.layout.Template()
     template.layout = dict(
         template="simple_white",
         paper_bgcolor=backgroundcolor,
         plot_bgcolor=backgroundcolor,
-        font=dict(size=14),
-        margin=dict(l=40, r=40, t=40, b=40, pad=4),
-        hoverlabel=dict(font_size=12),
+        font=dict(size=fontsize),
+        margin=dict(
+            l=50,
+            r=0,
+            t=0,
+            b=50,
+            pad=3,
+        ),
+        hoverlabel=dict(font_size=fontsize),
         xaxis=global_axis,
         yaxis=global_axis,
         scene=dict(
@@ -54,15 +82,32 @@ def build_house_template() -> go.layout.Template:
             xaxis=local_axis,
             yaxis=local_axis,
             zaxis=local_axis,
-            camera=dict(eye=dict(x=1.6, y=1.3, z=0.9)),
+            camera=dict(
+                eye=dict(
+                    x=1.6,
+                    y=1.3,
+                    z=0.9,
+                )
+            ),
         ),
     )
-    template.data.scatter = [go.Scatter(mode="lines", line=dict(width=2))]
+    template.data.scatter = [
+        go.Scatter(
+            mode="lines",
+            line=dict(width=linewidth),
+        ),
+    ]
     template.data.heatmap = [
-        go.Heatmap(colorscale=colormap, showscale=True),
+        go.Heatmap(
+            colorscale=colormap,
+            showscale=True,
+        ),
     ]
     template.data.surface = [
-        go.Surface(colorscale=colormap, showscale=False),
+        go.Surface(
+            colorscale=colormap,
+            showscale=False,
+        ),
     ]
 
     return template
@@ -78,5 +123,4 @@ def register_house_template(set_default: bool = True) -> None:
     """
     pio.templates["house"] = build_house_template()
     if set_default:
-        pio.templates.default = "house"
         pio.templates.default = "house"
