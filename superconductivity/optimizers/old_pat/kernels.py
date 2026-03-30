42057d6aa79d72124b025e4503504ef06d2de696
schmampf <oliver.irtenkauf@uni-konstanz.de>
2026-03-30 14:37:30 +0200

    rf old pat stuff

--- superconductivity/optimizers/old_pat/kernels.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from .models import get_model
-from ...utilities.types import NDArray64
-
-
-def pat_trace(V_mV: NDArray64, params: NDArray64) -> NDArray64:
-    function, parameter_mask = get_model(model="pat")
-    return function(V_mV, *params[parameter_mask])
