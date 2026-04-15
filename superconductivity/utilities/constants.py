6fba1d14c2201574ea48c7d52a4d7ad9cddc369e
schmampf <oliver.irtenkauf@uni-konstanz.de>
2026-04-15 13:37:44 +0200

    tighten constants

--- superconductivity/utilities/constants.py
+++ superconductivity/utilities/constants.py
@@ -10,79 +10,18 @@ from scipy.constants import Boltzmann as _kB
 from scipy.constants import e as _e
 from scipy.constants import h as _h
 
-from .meta.param import ParamSpec
+from .meta.param import param
 
-e = ParamSpec(
-    label="e",
-    html_label="<i>e</i> (As)",
-    latex_label=r"$e$ (As)",
-    value=float(_e),
-    fixed=True,
-)
+e = param("e", float(_e), fixed=True)
+h = param("h", float(_h), fixed=True)
+kB = param("kB", float(_kB), fixed=True)
 
-h = ParamSpec(
-    label="h",
-    html_label="<i>h</i> (J s)",
-    latex_label=r"$h$ (J s)",
-    value=float(_h),
-    fixed=True,
-)
-
-kB = ParamSpec(
-    label="kB",
-    html_label="<i>k</i><sub>B</sub> (J/K)",
-    latex_label=r"$k_B$ (J/K)",
-    value=float(_kB),
-    fixed=True,
-)
-
-G0_S = ParamSpec(
-    label="G0_S",
-    html_label="<i>G<sub>0</sub></i> (S)",
-    latex_label=r"$G_0$ (S)",
-    value=2.0 * e.value * e.value / h.value,
-    fixed=True,
-)
-
-h_Vs = ParamSpec(
-    label="h_Vs",
-    html_label="<i>h</i> (Vs)",
-    latex_label=r"$h$ (V s)",
-    value=h.value / e.value,
-    fixed=True,
-)
-
-kB_eV_K = ParamSpec(
-    label="kB_eV_K",
-    html_label="<i>k</i><sub>B</sub> (eV/K)",
-    latex_label=r"$k_B$ (eV/K)",
-    value=kB.value / e.value,
-    fixed=True,
-)
-
-G0_muS = ParamSpec(
-    label="G0_muS",
-    html_label="<i>G<sub>0</sub></i> (&micro;S)",
-    latex_label=r"$G_0$ (\mu S)",
-    value=float(G0_S.value) * 1e6,
-    fixed=True,
-)
-
-h_pVs = ParamSpec(
-    label="h_pVs",
-    html_label="<i>h</i> (pV s)",
-    latex_label=r"$h$ (pV s)",
-    value=h_Vs.value * 1e12,
-    fixed=True,
-)
-
-kB_meV_K = ParamSpec(
-    label="kB_meV_K",
-    html_label="<i>k</i><sub>B</sub> (meV/K)",
-    latex_label=r"$k_B$ (meV/K)",
-    value=kB_eV_K.value * 1e3,
-    fixed=True,
-)
+G0_S = param("G0_S", 2.0 * e * e / h, fixed=True)
+h_Vs = param("h_Vs", h / e, fixed=True)
+kB_eV_K = param("kB_eV_K", kB / e, fixed=True)
+G0_muS = param("G0_muS", G0_S * 1e6, fixed=True)
+h_pVs = param("h_pVs", h_Vs * 1e12, fixed=True)
+kB_meV_K = param("kB_meV_K", kB_eV_K * 1e3, fixed=True)
 
 # e = 1.602176634e-19 As
 # h = 6.62607015e-34 J s
